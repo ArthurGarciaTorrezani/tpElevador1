bc3ed62ca3177c437d885299729f82ce5f9e90f5
Arthur Garcia Torrezani <79718431+ArthurGarciaTorrezani@users.noreply.github.com>
2023-10-31 19:54:17 -0300

    Até a 6

--- Elevador.java
+++ Elevador.java
@@ -1,66 +1,79 @@
-public class Elevador
-{
+public class Elevador {
     // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
     private boolean porta;
     private boolean subir;
     private boolean descer;
     private Piso pisoAtual;
+    private Piso piso1;
 
-    public Elevador(Piso pisoAtual)
-    {
+    public Elevador(Piso pisoAtual) {
         this.pisoAtual = pisoAtual;
+        this.piso1 = pisoAtual;
         porta = false;
         subir = true;
     }
-    
-    public void abrirPorta(){
+
+    public void abrirPorta() {
         porta = true;
     }
-    
-    public void fecharPorta(){
+
+    public void fecharPorta() {
         porta = false;
     }
-    
-    public boolean isPortaAberta(){
+
+    public boolean isPortaAberta() {
         return porta;
     }
-    
-    public boolean isSubindo(){
+
+    public void subir(){
+        subir = true;
+        descer = false;
+    }
+
+    public void descer(){
+        subir = false;
+        descer = true;
+    }
+
+    public boolean isSubindo() {
         return subir;
     }
-    
-    public boolean isDescendo(){
+
+    public boolean isDescendo() {
         return descer;
     }
-    
-    public int getPisoAtual(){
+
+    public int getPisoAtual() {
         return pisoAtual.getAndar();
     }
-    
-    public void pararNoPiso(Piso pisoParar){
+
+    public void pararNoPiso(Piso pisoParar) {
         pisoParar.pararElevador();
     }
 
-   
-
-    public void mostrarPainel(){
-        Piso pisoReferencia = pisoAtual;
+    public void mostrarPainel() {
+        Piso pisoReferencia = piso1;
         System.out.println("*** Painel Elevador ***");
-        for(int i = 1;i <= pisoAtual.contador;i++){
-            
-            if(pisoReferencia == pisoAtual){
-                System.out.print(">"+i+"<");
-            }else if(pisoReferencia.isParadaSolicitada()){
-                System.out.print("*"+i+"*"); 
-            }else{
-                System.out.print(""+i+""); 
-            }
+        for (int i = 1; i <= pisoAtual.contador; i++) {
 
-            
-                pisoReferencia = pisoReferencia.getProximoPiso();
-            
-        }   
+            if(pisoReferencia == pisoAtual) {
+                System.out.print(">" + i + "<");
+            }else if (pisoReferencia.isParadaSolicitada()) {
+                System.out.print("*" + i + "*");
+            } else {
+                System.out.print("" + i + "");
+            }
+            pisoReferencia = pisoReferencia.getProximoPiso();
+        }
     }
 
-    
+    public void mover(){
+        if(subir){
+            pisoAtual = pisoAtual.getProximoPiso();
+        }else{
+            pisoAtual = pisoAtual.getPisoAnterior();
+        }
+        
+    }
+
 }
