7371a5c44b7b9aaa85c2c60234e1e740d5fdfc9b
Arthur Garcia Torrezani <79718431+ArthurGarciaTorrezani@users.noreply.github.com>
2023-11-01 13:50:02 -0300

    Completo Real

--- Elevador.java
+++ Elevador.java
@@ -1,104 +1,110 @@
-public class Elevador {
-    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
-    private boolean porta;
+public class Piso {
+    private int andar;
     private boolean subir;
     private boolean descer;
-    private Piso pisoAtual;
-    private Piso piso1;
+    private Piso proxPiso;
+    private Piso pisoAnte;
+    private boolean pararElevador;
+    private boolean paradaSolicitada;
+    public static int contador = 0;
+    private boolean receberElevador;
 
-    public Elevador(Piso pisoAtual) {
-        this.pisoAtual = pisoAtual;
-        this.piso1 = pisoAtual;
-        porta = false;
+    public Piso(int andar) {
+        this.andar = andar;
+        contador++;
+    }
+
+    public int getAndar() {
+        return andar;
+    }
+
+    public void queroSubir() {
         subir = true;
     }
 
-    public void abrirPorta() {
-        porta = true;
-    }
-
-    public void fecharPorta() {
-        porta = false;
-    }
-
-    public boolean isPortaAberta() {
-        return porta;
-    }
-
-    public void subir() {
-        subir = true;
-        descer = false;
-    }
-
-    public void descer() {
-        subir = false;
+    public void queroDescer() {
         descer = true;
     }
 
-    public boolean isSubindo() {
+    public void setSubirFalsa(){
+        subir = false;
+    }
+
+    public void setDescerFalsa(){
+        descer = false;
+    }
+
+    public boolean isQueroSubir() {
         return subir;
     }
 
-    public boolean isDescendo() {
+    public boolean isQueroDescer() {
         return descer;
     }
 
-    public int getPisoAtual() {
-        return pisoAtual.getAndar();
+    public void setProximoPiso(Piso proxPiso) {
+        this.proxPiso = proxPiso;
     }
 
-    public void pararNoPiso(Piso pisoParar) {
-        pisoParar.pararElevador();
+    public void setPisoAnterior(Piso pisoAnte) {
+        this.pisoAnte = pisoAnte;
     }
 
-    public void mostrarPainel() {
-        Piso pisoReferencia = piso1;
-        System.out.println("*** Painel Elevador ***");
-        for (int i = 1; i <= pisoAtual.contador; i++) {
-
-            if (pisoReferencia == pisoAtual) {
-                System.out.print(">" + i + "<");
-            } else if (pisoReferencia.isParadaSolicitada()) {
-                System.out.print("*" + i + "*");
-            } else {
-                System.out.print("" + i + "");
-            }
-            pisoReferencia = pisoReferencia.getProximoPiso();
-        }
+    public Piso getProximoPiso() {
+        return proxPiso;
     }
 
-    public void mover() {
-        if(!porta){
-            if(pisoAtual.getAndar() == pisoAtual.contador){
-                descer = true;
-                pisoAtual = pisoAtual.getPisoAnterior();
-            }else if(pisoAtual.getAndar() == 1){
-                subir = true;
-                pisoAtual = pisoAtual.getProximoPiso();
-            }else{
-                if(subir){
-                    pisoAtual.receberElevador(subir, descer);
-                    if(pisoAtual.getReceberElevador()){
-                        porta = true;
-                        pisoAtual.setSubirFalsa();
-                        pisoAtual.setParadaFalsa();
-                    }else{
-                        pisoAtual = pisoAtual.getProximoPiso();
-                    }
-                } else {
-                    pisoAtual.receberElevador(subir, descer);
-                    if(pisoAtual.getReceberElevador()){
-                        porta = true;
-                        pisoAtual.setDescerFalsa();
-                        pisoAtual.setParadaFalsa();
-                    }else{
-                        pisoAtual = pisoAtual.getPisoAnterior();
-                    }
-                }
+    public Piso getPisoAnterior() {
+        return pisoAnte;
+    }
 
-            }
+    public void pararElevador() {
+        pararElevador = true;
+    }
+
+    public boolean getPararElevador(){
+        return pararElevador;
+    }
+
+    public void solicitarParada() {
+        paradaSolicitada = true;
+    }
+
+    public void setParadaFalsa(){
+        paradaSolicitada = false;
+    }
+
+    public boolean isParadaSolicitada() {
+        return paradaSolicitada;
+    }
+
+    public void receberElevador(boolean elevadorSubindo, boolean elevadorDescendo) {
+        if (paradaSolicitada) {
+            receberElevador = true;
+        } else if (subir == elevadorSubindo) {
+            receberElevador = true;
+        } else if (descer == elevadorDescendo) {
+            receberElevador = true;
+        }else{
+            receberElevador = false;
         }
 
+    }
+
+    public boolean getReceberElevador(){
+        return receberElevador;
+    }
+    
+    public void mostrarPainel(){
+        if(subir && descer){
+            System.out.println(andar + " ▲▼");
+        }else if(!subir && !descer){
+            System.out.println(andar + " △▽");
+        }else if(subir && !descer){
+            System.out.println(andar+" ▲▽");
+        }else{
+            System.out.println(andar+" △▼");
+        }
     }
 
 }
